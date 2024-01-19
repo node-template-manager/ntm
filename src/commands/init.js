f6202d4ec4e90f1f988e6adef1c157097279bd41
csanbardev <csanbar97@gmail.com>
2024-01-19 17:18:31 +0100

    add new data to json file: metadata & dependencies

--- src/commands/init.js
+++ src/commands/init.js
@@ -9,15 +9,16 @@ const init = (argv) => {
 
   try {
     // execute npm init
-    //execSync('npm init -y', { cwd: path, stdio: 'inherit' })
+    execSync('npm init -y', { cwd: path, stdio: 'inherit' })
 
     const templatePath = path.join(__dirname, `../templates/${template}/${template}.json`)
     
     const templateData = readJSONFile(templatePath)
 
+    // create files & folders
     creator(templateData, projectPath, template)
   } catch (error) {
-
+    console.log('init function error: ',error)
   }
 }
 
