a87f8ddfe63f60d1689879072aaf9af2bde3bc74
csanbardev <csanbar97@gmail.com>
2024-01-22 13:21:13 +0100

    delete default command by yargs

--- src/index.js
+++ src/index.js
@@ -7,15 +7,6 @@ const { init } = require("./commands/init");
 yargs
   .scriptName("ntm")
   .usage('$0 <cmd> [args]')
-  .command('hello [name]', 'welcome ter yargs!', (yargs) => {
-    yargs.positional('name', {
-      type: 'string',
-      default: 'Cambi',
-      describe: 'the name to say hello to'
-    })
-  }, function (argv) {
-    console.log('hello', argv.name, 'welcome to yargs!')
-  })
   .command('init [template] [projectPath]', 'create a node project with a template', (yargs) => {
     yargs.positional(
       'template', {
