6c5ca5154db5d6818bd480d8379f1f532c191382
csanbardev <csanbar97@gmail.com>
2024-01-23 14:51:46 +0100

    implement create function

--- src/commands/create.js
+++ src/commands/create.js
@@ -1,5 +1,7 @@
 const { checkTemplate } = require("../utils/checkTemplate")
 const path = require('path')
+const { creator } = require("../utils/creator")
+const fse = require('fs-extra')
 
 
 
@@ -8,18 +10,36 @@ const create = (argv) => {
     const { name } = argv
 
     // template's name must not exists
-    if(checkTemplate){
+    if(checkTemplate()){
       throw Error("name already exists")
     }
 
-    const newTemplatePath = path.join(__dirname, `../templates/${template}/`)
+    const newTemplatePath = path.join(__dirname, `../templates/${name}`)
 
+    const createTemplatePah = path.join(__dirname, `../config/create/create.json`)
+
+    const templateData = readJSONFile(createTemplatePah)
+
+
+    creator(templateData.template, newTemplatePath, name)
 
 
   } catch (error) {
     console.log('ntm create error -> ', error.message)
   }
 }
+
+const readJSONFile = (path) => {
+  try {
+    const content = fse.readFileSync(path, 'utf-8')
+    const data = JSON.parse(content)
+    return data
+  } catch (error) {
+    console.error('Error al leer el archivo JSON:', error.message);
+    return null;
+  }
+}
+
 
 module.exports = {
   create
